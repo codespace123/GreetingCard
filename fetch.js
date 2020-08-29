49ff787cd70a734c5d831591fb1a29f56a20873b
codespace123 <47966827+codespace123@users.noreply.github.com>
2020-08-29 23:41:14 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,1 +1,4 @@
 console.log("version: " + Web3.version);
+web3 = new Web3(new Web3.providers.HttpProvider("https://mainnet.infura.io/v3/5334d7957c024b359dcb1dbc7275c4cd"));
+web3.eth.getTransaction('0xac1f94d742ebe49e86a37d37298388744b82548d353a709bfcc57bf7e85d5d1c').then(console.log);
+
