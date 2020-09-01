c273cb048bc0eef8e594b28f3b195be939be8945
codespace123 <47966827+codespace123@users.noreply.github.com>
2020-09-01 23:31:41 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,9 +1,12 @@
 console.log("version: " + Web3.version);
 async function myFetch() {
-  var web3 = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/5334d7957c024b359dcb1dbc7275c4cd"));
+  let web3 = await new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/5334d7957c024b359dcb1dbc7275c4cd"));
   let myEntry  = await web3.eth.getTransaction('0x51d2baa72696379f25477566014a1d6e417ec7a4b79bf63b52077200b47812e9');
   console.log(myEntry.input);
+  return {
+        myEntry
+  };
 }
-
-myFetch();
-
+myFetch().then(function(data) {
+    console.log('all is complted', data.input);
+});
