7a9868d242717f1bdaec284e1d1d3d3dc8383a2f
codespace123 <47966827+codespace123@users.noreply.github.com>
2020-09-01 22:58:00 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,5 +1,7 @@
 console.log("version: " + Web3.version);
 var web3 = new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/5334d7957c024b359dcb1dbc7275c4cd"));
-var a = web3.eth.getTransaction('0x51d2baa72696379f25477566014a1d6e417ec7a4b79bf63b52077200b47812e9');
-console.log(a.input);
+let myEntry  = await web3.eth.getTransaction('0x51d2baa72696379f25477566014a1d6e417ec7a4b79bf63b52077200b47812e9');
+var myEntryObject
+myEntry.then(a => myEntryObject = a)
+console.log(myEntryObject[0];
 
