25784ca5400e95fc2f3fcdb565633ecc77e343ac
codespace123 <47966827+codespace123@users.noreply.github.com>
2020-09-01 23:43:14 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -3,6 +3,7 @@ async function myFetch() {
   let web3 = await new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/5334d7957c024b359dcb1dbc7275c4cd"));
   let myEntry  = await web3.eth.getTransaction('0x51d2baa72696379f25477566014a1d6e417ec7a4b79bf63b52077200b47812e9');
   console.log(myEntry.input);
+   $("#text").html("Text added by jQuery code."+myEntry.input);
   return {
         myEntry
   };
