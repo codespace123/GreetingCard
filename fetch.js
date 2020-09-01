2f3f484fcbded508bee916f44dfeb3c394b35d8c
codespace123 <47966827+codespace123@users.noreply.github.com>
2020-09-01 23:59:47 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,9 +1,18 @@
 console.log("version: " + Web3.version);
+function fromHex(h) {
+    var s = ''
+    for (var i = 0; i < h.length; i+=2) {
+        s += String.fromCharCode(parseInt(h.substr(i, 2), 16))
+    }
+    return decodeURIComponent(escape(s));
+}
+
 async function myFetch() {
   let web3 = await new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/5334d7957c024b359dcb1dbc7275c4cd"));
   let myEntry  = await web3.eth.getTransaction('0x51d2baa72696379f25477566014a1d6e417ec7a4b79bf63b52077200b47812e9');
   console.log(myEntry.input);
-  document.getElementById("text").innerHTML = "Text added by JavaScript code"+myEntry.input;
+  let x = await fromHex(myEntry.input.substring(2));
+  document.getElementById("text").innerHTML = x;
   return {
         myEntry
   };
