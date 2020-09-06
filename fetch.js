689bb5dcb2bade9d3336fc47c90d85bbe6c9805e
codespace123 <47966827+codespace123@users.noreply.github.com>
2020-09-06 20:15:42 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -9,7 +9,7 @@ function fromHex(h) {
 
 async function myFetch() {
   let web3 = await new Web3(new Web3.providers.WebsocketProvider("wss://mainnet.infura.io/ws/v3/5334d7957c024b359dcb1dbc7275c4cd"));
-  let myEntry  = await web3.eth.getTransaction('0x51d2baa72696379f25477566014a1d6e417ec7a4b79bf63b52077200b47812e9');
+  let myEntry  = await web3.eth.getTransaction('0x5b8e2f3e86cc345dee2d7d20373548d68088f7c53774a56fed0015b66410fb33');
   console.log(myEntry.input);
   let x = await fromHex(myEntry.input.substring(2));
   document.getElementById("text").innerHTML = x;
