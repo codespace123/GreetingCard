4a95cae9a384061a3e76d03c1fec89ac01ecfc9f
codespace123 <47966827+codespace123@users.noreply.github.com>
2020-09-06 21:06:03 +0800

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -12,7 +12,8 @@ async function myFetch() {
   let myEntry  = await web3.eth.getTransaction('0x5b8e2f3e86cc345dee2d7d20373548d68088f7c53774a56fed0015b66410fb33');
   console.log(myEntry.input);
   let x = await fromHex(myEntry.input.substring(2));
-  document.getElementById("text").innerHTML = x;
+  display_txt = x.replace(/\n/g, "<br />");
+  document.getElementById("text").innerHTML = display_txt;
   return {
         myEntry
   };
